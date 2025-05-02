53bacf143f35e9bd7099d7fdb679b543ff88e628
Kate Jang <bobaejang1@gmail.com>
2025-05-02 15:04:39 +0900

    fix: useRouter 버그 수정

--- src/components/navbar/NavModal.tsx
+++ src/components/navbar/NavModal.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import { MouseEvent } from "react"
 import styled from "styled-components";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 
 const Overlay = styled.div`
