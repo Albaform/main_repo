3734786337d89cd721e346ae9b495527b2e21a32
Kate Jang <bobaejang1@gmail.com>
2025-05-05 21:00:51 +0900

    feat: 최신순 드랍다운 메뉴 구현 완

--- src/app/albaformList/page.tsx
+++ src/app/albaformList/page.tsx
@@ -3,9 +3,13 @@
 import AlbaList from "./components/AlbaList";
 import ListFilter from "./components/ListFilter";
 import SearchBar from "./components/SearchBar";
+import { useState } from "react";
+
 
 export default function AlbaFormList(){
-
+const [isSort, setIsSort] = useState<
+    'mostRecent' | 'mostCommented' | 'mostLiked'
+  >('mostRecent');
 
 
     return(
