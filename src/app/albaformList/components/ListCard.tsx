cc73a8a2850841f4cd5a3b2f1a4fa2ed8b5828a0
Kate Jang <bobaejang1@gmail.com>
2025-05-05 23:12:27 +0900

    fix: 알바폼 리스트 fallback이미지 코드잇 이미지 적용

--- src/app/albaformList/components/ListCard.tsx
+++ src/app/albaformList/components/ListCard.tsx
@@ -4,9 +4,57 @@ import { BannerImg, BottomCard, BottomSectionFirst, BottomSectionSecond, BottomS
 import Image from "next/image";
 
 
-export default function ListCard(){
-    const bannerImg = '/images/albaformList/image 7.png';
+interface ListCardProps {
+    form: FormData; 
+  }
+  
+
+export default function ListCard({ form }: ListCardProps){
+
+    const [imgSrc, setImgSrc] = useState(form.imageUrls[0] || '/images/albaformList/image 7.png');
     const kebabIcon = '/images/albaformList/kebab-menu.png';
+
+
+    //fallback이미지
+    const handleError = () => {
+        setImgSrc('/images/albaformList/image 7.png'); // fallback 이미지
+      };
+    //마감일 계산 
+
+    function getDday(recruitmentEndDate: string): string {
+        const today = new Date();
+        const endDate = new Date(recruitmentEndDate);
+    
+        // 오늘 날짜의 시간을 00:00:00으로 리셋
+        today.setHours(0, 0, 0, 0);
+        endDate.setHours(0, 0, 0, 0);
+    
+        const diffTime = endDate.getTime() - today.getTime();
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    
+        if (diffDays < 0) return '마감';
+        if (diffDays === 0) return 'D-DAY';
+        return `D-${diffDays}`;
+    }
+
+    //모집중 계산
+  function getRecruitStatus(startDate: string, endDate: string): '모집중' | '모집 예정' | '모집 마감' {
+        const today = new Date();
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+      
+        // 날짜만 비교
+        today.setHours(0, 0, 0, 0);
+        start.setHours(0, 0, 0, 0);
+        end.setHours(0, 0, 0, 0);
+      
+        if (today < start) return '모집 예정';
+        if (today > end) return '모집 마감';
+        return '모집중';
+      }
+    
+    //fallback 이미지 함수
+  
 
     return(
 
