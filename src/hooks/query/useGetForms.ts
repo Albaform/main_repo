4b1968858ff5af9c958ec905de3aa2a2c9531379
Kate Jang <bobaejang1@gmail.com>
2025-05-06 17:17:35 +0900

    fix: fetch함수와 query함수 분리

--- src/hooks/query/useGetForms.ts
+++ src/hooks/query/useGetForms.ts
@@ -3,15 +3,9 @@
 
 // 예시: useQuery 내부에서 직접 axios 인스턴스 사용
 import { useQuery } from '@tanstack/react-query';
-import instance from '@/lib/api/api';
-import { FormListResponse } from '@/app/albaformList/types';
-
-
-  export const fetchAlbaForms = async (limit = 9): Promise<FormListResponse> => {
-    const res = await instance.get(`/forms?limit=${limit}`);
-    return res.data;
-  };
+import { fetchAlbaForms } from '@/lib/fetch/form';
   
+
 
   export function useAlbaForms(limit = 9) {
     return useQuery({
