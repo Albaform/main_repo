3937a449bfafb51a18aa83103566a921b2c33578
HyeyeonJung <jhy20306@gmail.com>
2025-06-11 17:14:12 +0900

    fix: mutation rollback

--- src/hooks/mutation/useUploadImage.ts
+++ src/hooks/mutation/useUploadImage.ts
@@ -1,27 +1,11 @@
 // 이미지 bin파일 랜더링
 // POST 'images/upload'
 
+import { fetchUploadImage } from '@/lib/fetch/file';
 import { useMutation } from '@tanstack/react-query';
 
-const uploadImage = async (file: File): Promise<string> => {
-  const formData = new FormData();
-  formData.append('file', file);
-  console.log('업로드할 파일:', file);
-
-  const response = await fetch('/images/upload', {
-    method: 'POST',
-    body: formData,
+export const useUploadImage = () => {
+  return useMutation({
+    mutationFn: fetchUploadImage,
   });
-
-  if (!response.ok) {
-    throw new Error('이미지 업로드 실패');
-  }
-
-  const data = await response.json();
-  return data.url;
 };
-
-export const useImageUpload = () =>
-  useMutation({
-    mutationFn: uploadImage,
-  });
