680efcbe5835f4ce8dd25615cefdadf13ccb364b
Nerim <nerim5037@gmail.com>
2025-06-17 16:20:25 +0900

    feat: 사장님 - 알바폼 지원자 상세페이지 UI 구조 및 데이터 출력

---
+++ src/app/applications/[id]/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useGetApplicationsById } from '@/hooks/query/useGetApplicationsById';
+import { useGetFormsById } from '@/hooks/query/useGetFormsById';
+import { useParams, useSearchParams } from 'next/navigation';
+import Section1 from './components/section1/Section1';
+import Section2 from './components/section2/Section2';
+import { ResponsiveStyle } from '@/styles/responsiveStyle';
+import Loader from '@/components/loader/Loader';
+import { DetailResponsive } from './styles';
+
+export default function ApplicationDetail() {
+  const searchParams = useSearchParams();
+  const searchParam = searchParams.get('formId');
+  const formId = Number(searchParam);
+  const params = useParams();
+  const paramsId = Array.isArray(params.id) ? params.id[0] : params.id ?? '';
+  const applicationId = Number(paramsId);
+
+  const { data: form, isLoading: getFormLoading } = useGetFormsById(formId);
+  const { data: application, isLoading: getApplicationLoading } =
+    useGetApplicationsById(formId, applicationId);
+
+  const isLoading = getFormLoading || getApplicationLoading;
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <DetailResponsive>
+        <Section1 formData={form} />
+      </DetailResponsive>
+      <div className='h-3 bg-line-100' />
+      <DetailResponsive>
+        <Section2 applicationData={application} />
+      </DetailResponsive>
+    </>
+  );
+}
---
+++ src/app/applications/[id]/types.ts
@@ -0,0 +1,13 @@
+export interface DetailApplicationDataProps {
+  id: number;
+  name: string;
+  phoneNumber: string;
+  experienceMonths: number;
+  resumeId: number;
+  resumeName: string;
+  introduction: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  applicantId: number;
+}
